2692f56b821838a46e732b61f2ecf733eda50d1c
Wei <mai@mai0313.com>
2023-11-10 00:01:16 +0800

    Update Python version to 3.9.15 in notebooks

--- exrate.py
+++ exrate.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 
 
 def get_country_list():
-    with open('./configs/countries_original.json', 'rb') as file:
+    with open("./configs/countries_original.json", "rb") as file:
         json_data = file.read()
         data = orjson.loads(json_data)
     return data
@@ -36,7 +36,9 @@ def get_default_country(target_country, output_path):
             visa = driver.find_element(By.ID, "comparison_huilv_Visa").text
             visadate = driver.find_element(By.CSS_SELECTOR, ".even .gray_font").text
             visadate = visadate.replace("(", "").replace(")", "")
-            master = driver.find_element(By.CSS_SELECTOR, ".odd:nth-child(1) > td:nth-child(2)").text
+            master = driver.find_element(
+                By.CSS_SELECTOR, ".odd:nth-child(1) > td:nth-child(2)"
+            ).text
             masterdate = master.split(" ")[-1].replace("(", "").replace(")", "")
             master = master.split(" ")[0]
             jcb = driver.find_element(By.CSS_SELECTOR, ".odd:nth-child(3) > td:nth-child(2)").text
@@ -44,18 +46,20 @@ def get_default_country(target_country, output_path):
             jcb = jcb.split(" ")[0]
         except NoSuchElementException:
             pass
-        data = [{
-            "國家": country_in_chinese,
-            "幣值": country,
-            "金額": 2990,
-            "更新時間": visadate,
-            "Visa匯率": visa,
-            "Visa 試算結果": f"=C{func_num}*E{func_num}*1.5",
-            "Master匯率": master,
-            "Master 試算結果": f"=C{func_num}*H{func_num}*1.5",
-            "JCB匯率": jcb,
-            "JCB 試算結果": f"=C{func_num}*K{func_num}*1.5",
-        }]
+        data = [
+            {
+                "國家": country_in_chinese,
+                "幣值": country,
+                "金額": 2990,
+                "更新時間": visadate,
+                "Visa匯率": visa,
+                "Visa 試算結果": f"=C{func_num}*E{func_num}*1.5",
+                "Master匯率": master,
+                "Master 試算結果": f"=C{func_num}*H{func_num}*1.5",
+                "JCB匯率": jcb,
+                "JCB 試算結果": f"=C{func_num}*K{func_num}*1.5",
+            }
+        ]
         data = pd.DataFrame(data)
         result = pd.concat([result, data], axis=0, ignore_index=True)
         print(country_in_chinese, "has been done")
