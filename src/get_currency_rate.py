0d301107403f7bacb40cbd85da70b22444e5090b
Wei <mai@mai0313.com>
2023-11-10 06:14:31 +0800

    Add progress bar to currency rate scraper

--- src/get_currency_rate.py
+++ src/get_currency_rate.py
@@ -4,6 +4,7 @@ import re
 import orjson
 import pandas as pd
 from playwright.sync_api import sync_playwright
+from rich.progress import Progress
 
 
 def get_country_list():
@@ -51,50 +52,52 @@ def get_default_country(browser, target_country, output_path):
 
     result = pd.DataFrame()
     result_csv = pd.DataFrame()
-    for i, (country, country_in_chinese) in enumerate(target_country.items()):
-        func_num = i + 2
-        target_url = f"https://www.bestxrate.com/card/mastercard/{country}.html"
-        page.goto(target_url, timeout=5000)
+    with Progress() as progress:
+        task1 = progress.add_task("[green]Downloading...", total=len(target_country))
+        for i, (country, country_in_chinese) in enumerate(target_country.items()):
+            progress.update(task1, advance=1, description=f"[cyan]Downloading {country} {country_in_chinese}")
+            func_num = i + 2
+            target_url = f"https://www.bestxrate.com/card/mastercard/{country}.html"
 
-        try:
-            visa = page.locator("#comparison_huilv_Visa").text_content()
-            master_info = page.locator(".odd:nth-child(1) > td:nth-child(2)").text_content()
-            master = get_date(master_info)
-            master, update_date = master.split("\xa0")
-            jcb = page.locator("#comparison_huilv_JCB").text_content()
-            data = [
-                {
-                    "國家": country_in_chinese,
-                    "幣值": country,
-                    "金額": 2990,
-                    "更新時間": update_date,
-                    "Visa匯率": visa,
-                    "Visa 試算結果": f"=C{func_num}*E{func_num}*1.5",
-                    "Master匯率": master,
-                    "Master 試算結果": f"=C{func_num}*H{func_num}*1.5",
-                    "JCB匯率": jcb,
-                    "JCB 試算結果": f"=C{func_num}*K{func_num}*1.5",
-                }
-            ]
-            data = pd.DataFrame(data)
-            result = pd.concat([result, data], axis=0, ignore_index=True)
+            try:
+                page.goto(target_url, timeout=5000)
+                visa = page.locator("#comparison_huilv_Visa").text_content()
+                master_info = page.locator(".odd:nth-child(1) > td:nth-child(2)").text_content()
+                master = get_date(master_info)
+                master, update_date = master.split("\xa0")
+                jcb = page.locator("#comparison_huilv_JCB").text_content()
+                data = [
+                    {
+                        "國家": country_in_chinese,
+                        "幣值": country,
+                        "金額": 2990,
+                        "更新時間": update_date,
+                        "Visa匯率": visa,
+                        "Visa 試算結果": f"=C{func_num}*E{func_num}*1.5",
+                        "Master匯率": master,
+                        "Master 試算結果": f"=C{func_num}*H{func_num}*1.5",
+                        "JCB匯率": jcb,
+                        "JCB 試算結果": f"=C{func_num}*K{func_num}*1.5",
+                    }
+                ]
+                data = pd.DataFrame(data)
+                result = pd.concat([result, data], axis=0, ignore_index=True)
 
-            data_csv = [
-                {
-                    "國家": country_in_chinese,
-                    "幣值": country,
-                    "更新時間": update_date,
-                    "Visa匯率": visa,
-                    "Master匯率": master,
-                    "JCB匯率": jcb,
-                }
-            ]
-            data_csv = pd.DataFrame(data_csv)
-            result_csv = pd.concat([result_csv, data_csv], axis=0, ignore_index=True)
-            print(country_in_chinese, "has been done")
-        except Exception as e:
-            print(f"{country_in_chinese} has an error, please check {target_url}")
-            continue
+                data_csv = [
+                    {
+                        "國家": country_in_chinese,
+                        "幣值": country,
+                        "更新時間": update_date,
+                        "Visa匯率": visa,
+                        "Master匯率": master,
+                        "JCB匯率": jcb,
+                    }
+                ]
+                data_csv = pd.DataFrame(data_csv)
+                result_csv = pd.concat([result_csv, data_csv], axis=0, ignore_index=True)
+            except Exception as e:
+                print(f"{country_in_chinese} has an error, please check {target_url}")
+                pass
 
     result.to_excel(f"{output_path}/即時匯率.xlsx", index=False)
     browser.close()
@@ -103,7 +106,7 @@ def get_default_country(browser, target_country, output_path):
 
 if __name__ == "__main__":
     countries = get_country_list()
-    output_path = "data"
+    output_path = "data/currency_rate"
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         get_default_country(browser, countries, output_path)
