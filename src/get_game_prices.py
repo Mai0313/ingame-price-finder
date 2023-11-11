48e409a697229bb5a2c8349e42a481c5062b7735
Wei <21039366+Mai0313@users.noreply.github.com>
2023-11-11 22:42:53 +0000

    Refactor progress bar to use tqdm

--- src/get_game_prices.py
+++ src/get_game_prices.py
@@ -6,7 +6,7 @@ import pandas as pd
 from google_play_scraper import app
 from omegaconf import OmegaConf
 from pydantic import BaseModel
-from rich.progress import Progress
+from tqdm import tqdm
 
 
 def get_game_list():
@@ -37,30 +37,28 @@ class GamePriceProcessor(BaseModel):
     country: str
     country_name: str
 
-    def get_game_price_v1(self, progress):
+    def get_game_price_v1(self):
         games = get_game_list()
         results = []
-        task = progress.add_task(f"Processing {self.country}...", total=len(games))
-        for game in games:
+        for game in tqdm(games, desc=f"Processing {self.country}"):
             input_args = (game, self.country)
             name, country, price = get_game_info(input_args)
             data = {"遊戲名稱": name, "國家": country, "價格": price}
             results.append(data)
-            progress.update(task, advance=1)
         price_df = pd.DataFrame(results)
         price_df["價格"] = price_df["價格"].str.split("-").str[-1].str.split(" ").str[1]
         return price_df
 
-    def get_game_price_v2(self, progress):
+    def get_game_price_v2(self):
         games = get_game_list()
         with ProcessPoolExecutor() as executor:
             tasks = [executor.submit(get_game_info, (game, self.country)) for game in games]
-            task = progress.add_task(f"Processing {self.country_name}...", total=len(tasks))
-            results = []
-            for future in as_completed(tasks):
-                result = future.result()
-                results.append(result)
-                progress.update(task, advance=1)
+            results = [
+                r.result()
+                for r in tqdm(
+                    as_completed(tasks), total=len(tasks), desc=f"Processing {self.country_name}"
+                )
+            ]
         price_df = pd.DataFrame(results, columns=["遊戲名稱", "國家", "價格"])
         price_df["價格"] = price_df["價格"].str.split("-").str[-1].str.split(" ").str[1]
         return price_df
@@ -72,35 +70,30 @@ class GamePriceGrabber(BaseModel):
     parallel_games: bool
     output_path: str
 
-    def _get_game_info(self, country, progress):
+    def _get_game_info(self, country):
         currency_name = country["currencyName"]
         country_name = country["countryName"]
         processor = GamePriceProcessor(country=currency_name, country_name=country_name)
         if self.parallel_games:
-            result = processor.get_game_price_v2(progress)
+            result = processor.get_game_price_v2()
         else:
-            result = processor.get_game_price_v1(progress)
+            result = processor.get_game_price_v1()
         result.to_csv(
             f"{self.output_path!s}/{country_name}_info.csv", encoding="utf-8", index=None
         )
 
     def get_game_info_to_csv(self):
         os.makedirs(f"{self.output_path!s}", exist_ok=True)
-        with Progress() as progress:
-            total_task = progress.add_task("Overall Progress...", total=len(self.countries))
-
-            if self.parallel_countries:
-                with ProcessPoolExecutor() as executor:
-                    tasks = [
-                        executor.submit(self._get_game_info, country, progress)
-                        for country in self.countries
-                    ]
-                    for _ in as_completed(tasks):
-                        progress.update(total_task, advance=1)
-            else:
-                for country in self.countries:
-                    self._get_game_info(country, progress)
-                    progress.update(total_task, advance=1)
+        if self.parallel_countries:
+            with ProcessPoolExecutor() as executor:
+                tasks = [
+                    executor.submit(self._get_game_info, country) for country in self.countries
+                ]
+                for _ in tqdm(as_completed(tasks), total=len(tasks), desc="Processing countries"):
+                    pass
+        else:
+            for country in self.countries:
+                self._get_game_info(country)
 
 
 if __name__ == "__main__":
