727b622e647896eea32a7e92bd164a260570f4c5
Wei <mai@mai0313.com>
2024-03-01 02:57:33 +0800

    Add filtering and default value to selected_game dropdown

--- index.py
+++ index.py
@@ -15,6 +15,7 @@ def filter_games(selected_game):
         search_dict = orjson.loads(f.read())  # type: dict[str, str]
     game_info_path = f"./data/final_data/{search_dict[selected_game]}.csv"
     game_info_data = pd.read_csv(game_info_path)
+    game_info_data = game_info_data.query("JCB > 0 and 萬事達 > 0 and VISA > 0")
     game_info_data = game_info_data.sort_values(by=["JCB", "萬事達", "VISA"], ascending=True)
     return game_info_data
 
@@ -22,7 +23,7 @@ def filter_games(selected_game):
 search_list = game_list_keys()
 
 with gr.Blocks() as demo:
-    selected_game = gr.Dropdown(choices=search_list, label="選擇遊戲")
+    selected_game = gr.Dropdown(choices=search_list, label="選擇遊戲", value="崩壞：星穹鐵道")
     game_info = gr.DataFrame(label="價格訊息")
 
     def update_game_info(selected_game):
@@ -30,4 +31,5 @@ with gr.Blocks() as demo:
 
     selected_game.change(update_game_info, inputs=[selected_game], outputs=[game_info])
 
-demo.launch()
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=8081)
