d5138ba595fe478312e6475be7478337c684bafb
wei <mai@mai0313.com>
2024-09-13 00:03:17 +0800

    refactor: add logfire dependency for currency_core.py
    Signed-off-by: Wei <mai@mai0313.com>

--- src/utils/currency_core.py
+++ src/utils/currency_core.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+import bs4
 from pydantic import Field, BaseModel
 import requests
 
@@ -7,7 +7,7 @@ class CurrencyCore(BaseModel):
     country_name: str = Field(..., title="Country Name", description="Country name")
 
     @staticmethod
-    def parse_exchange_rate_info(exchange_rate_info: str) -> tuple[str, str]:
+    def __parse_exchange_rate_info(exchange_rate_info: str) -> tuple[str, str]:
         if "(" in exchange_rate_info:
             exchange_rate, date_info = exchange_rate_info.split("(")
             date_info = date_info.replace(")", "").strip()
@@ -16,9 +16,9 @@ class CurrencyCore(BaseModel):
             date_info = ""
         return exchange_rate, date_info
 
-    def parse_currency_rates(self, html_content: str) -> list[dict[str, str | None]]:
-        soup = BeautifulSoup(html_content, "html.parser")
-        rows = soup.find_all("tr")
+    def __parse_currency_rates(self, html_content: str) -> list[dict[str, str | None]]:
+        soup = bs4.BeautifulSoup(html_content, "html.parser")
+        rows: list[bs4.element.Tag] = soup.find_all("tr")
         currency_content = soup.find("a", onclick="change_ccy()")
         country_rates = {}
         currency_name = "未找到幣值名稱"
@@ -31,11 +31,11 @@ class CurrencyCore(BaseModel):
             )
 
         for row in rows:
-            cols = row.find_all("td")
-            if len(cols) > 1:
-                card_type = cols[0].text.strip()
-                exchange_rate_info = cols[1].text.strip()
-                exchange_rate, date_info = self.parse_exchange_rate_info(exchange_rate_info)
+            _cols = row.find_all("td")
+            if len(_cols) > 1:
+                card_type: str = _cols[0].text.strip()
+                exchange_rate_info: str = _cols[1].text.strip()
+                exchange_rate, date_info = self.__parse_exchange_rate_info(exchange_rate_info)
                 exchange_rate = exchange_rate.replace("\xa0", "").strip()
 
                 if self.country_name not in country_rates:
@@ -48,15 +48,11 @@ class CurrencyCore(BaseModel):
                         "VISA": None,
                     }
                 country_rates[self.country_name][card_type] = exchange_rate
-
         return country_rates.values()
 
     def fetch_currency_rates(self) -> list[dict[str, str | None]]:
         url = f"https://www.twrates.com/card/mastercard/{self.country_name}.html"
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            result = self.parse_currency_rates(response.text)
-            return result
-        except Exception:
-            pass
+        response = requests.get(url)
+        response.raise_for_status()
+        result = self.__parse_currency_rates(response.text)
+        return result
