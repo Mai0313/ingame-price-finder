23851909343c5c1a6f884d8d8d041e6111fb98a7
wei <mai@mai0313.com>
2024-09-13 23:30:32 +0800

    feat: enhance currency management and licensing
    ## What does this PR do?
    
    - Add MIT License file
    - Modify handling of currencies to support fetching rates for all currencies
    - Refactor currency fetching logic in `main.py` and `currency_core.py`
    - Include currency model dumping feature to format fetched data
    - Update project metadata to specify MIT license in `pyproject.toml`
    
    Signed-off-by: wei <mai@mai0313.com>

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from src.ingame_price import GameInfoUpdater
 from src.currency_core import CurrencyCore
 
 if __name__ == "__main__":
-    currency_rate = CurrencyCore(country_name="usd")
-    currency_rate.fetch_currency_rates()
-    currency_rate.get_avali_currency()
+    currency_rate = CurrencyCore(country_name="all")
+    currency_rate_list = currency_rate.fetch_currency_rates()
+    currency_rate_list = [f.model_dump() for f in currency_rate_list]
     game_info_fetcher = GameInfoUpdater(game_name="天堂W", country="us")
-    game_info_fetcher.fetch_game_info()
+    game_info_result = game_info_fetcher.fetch_game_info()
+    game_info_result = [f.model_dump() for f in game_info_result]
--- src/currency_core.py
+++ src/currency_core.py
@@ -9,12 +9,48 @@ logfire.configure()
 
 
 class CurrencyCore(BaseModel):
-    country_name: str = Field(default="usd", title="Country Name", description="Country name")
+    country_name: str = Field(
+        default="usd",
+        title="Country Name",
+        description="Country name, if all, it will fetch all.",
+        examples=["usd", "all"],
+    )
 
     @computed_field
     @property
     def base_url(self) -> str:
         return f"https://www.twrates.com/card/mastercard/{self.country_name}.html"
+
+    @computed_field
+    @property
+    def country_list(self) -> list[dict[str, str]]:
+        """Retrieves a list of all available currencies and their corresponding links.
+
+        Returns:
+            A list of dictionaries, where each dictionary contains the currency name (in Chinese) as the key and the currency link as the value.
+
+        Example:
+            >>> currency_rate = CurrencyCore()
+            >>> all_currency = currency_rate.country_list
+            >>> print(all_currency[0])
+            {'usd': {'美金': 'https://www.twrates.com/card/mastercard/usd.html'}}
+        """
+        # Use this as base url to get all available currency
+        response = requests.get(self.base_url)
+        if response.status_code == 200:
+            soup = bs4.BeautifulSoup(response.text, "html.parser")
+            __currency_list: bs4.element.Tag = soup.find_all("li", class_="itm")
+            currency_list = []
+            for currency in __currency_list:
+                currency_name = currency.text.strip()
+                currency_name_en = currency_name.split(" - ")[0].lower()
+                currency_name_cn = currency_name.split(" - ")[-1]
+                _currency_link = currency.find("a")["href"]
+                currency_link = f"https://www.twrates.com{_currency_link}"
+                currency_list.append({currency_name_en: {currency_name_cn: currency_link}})
+        else:
+            logfire.exception("Failed to get currency list", url=self.base_url)
+        return currency_list
 
     @staticmethod
     def __parse_exchange_rate_info(exchange_rate_info: str) -> CurrencyInfo:
@@ -26,35 +62,7 @@ class CurrencyCore(BaseModel):
         exchange_rate = exchange_rate.replace("\xa0", "").strip()
         return CurrencyInfo(exchange_rate=exchange_rate, date_info=date_info)
 
-    def get_avali_currency(self) -> list[dict[str, str]]:
-        """Retrieves a list of all available currencies and their corresponding links.
-
-        Returns:
-            A list of dictionaries, where each dictionary contains the currency name (in Chinese) as the key and the currency link as the value.
-
-        Example:
-            >>> currency_rate = CurrencyCore(country_name="usd")
-            >>> all_currency = currency_rate.get_avali_currency()[0]
-            >>> print(all_currency)
-            {'美金': 'https://www.twrates.com/card/mastercard/usd.html'}
-        """
-        # Use this as base url to get all available currency
-        response = requests.get(self.base_url)
-        if response.status_code == 200:
-            soup = bs4.BeautifulSoup(response.text, "html.parser")
-            __currency_list: bs4.element.Tag = soup.find_all("li", class_="itm")
-            currency_list = []
-            for currency in __currency_list:
-                currency_name = currency.text.strip()
-                currency_name_cn = currency_name.split(" - ")[-1]
-                _currency_link = currency.find("a")["href"]
-                currency_link = f"https://www.twrates.com{_currency_link}"
-                currency_list.append({currency_name_cn: currency_link})
-        else:
-            logfire.exception("Failed to get currency list", url=self.base_url)
-        return currency_list
-
-    def fetch_currency_rates(self) -> CurrencyRate:
+    def __fetch(self) -> CurrencyRate:
         """Fetches the currency rates for a specific country.
 
         Returns:
@@ -85,17 +93,28 @@ class CurrencyCore(BaseModel):
             )
         result = {"currency_en": self.country_name, "currency_cn": currency_name}
         for row in rows:
-            _cols = row.find_all("td")
+            _cols: bs4.element.ResultSet = row.find_all("td")
             if len(_cols) > 1:
                 card_type: str = _cols[0].text.strip()
                 exchange_rate_info: str = _cols[1].text.strip()
                 currency_info = self.__parse_exchange_rate_info(exchange_rate_info)
                 result[card_type] = currency_info.exchange_rate
         result["updated_time"] = currency_info.date_info
+        logfire.info("Currency rate fetched successfully.", **result)
         return CurrencyRate(**result)
+
+    def fetch_currency_rates(self) -> list[CurrencyRate]:
+        currency_rate_list = []
+        if self.country_name != "all":
+            currency_rate_list.append(self.__fetch())
+        else:
+            for currency_dict in self.country_list:
+                for currency_name_en in currency_dict:
+                    self.country_name = currency_name_en
+                    currency_rate_list.append(self.__fetch())
+        return currency_rate_list
 
 
 if __name__ == "__main__":
-    currency_rate = CurrencyCore(country_name="usd")
+    currency_rate = CurrencyCore(country_name="all")
     currency_rate.fetch_currency_rates()
-    currency_rate.get_avali_currency()
